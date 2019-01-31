8eb0964063370234cb759f87e68179570fd4516a
anubhavhuria <anubhavh@mindfiresolutions.com>
2019-01-31 13:23:35 +0530

    Changes in admin views and applying filters

--- adoption/admin.py
+++ adoption/admin.py
@@ -6,8 +6,15 @@ from .models import Vaccine
 
 @admin.register(Pet)
 class PetAdmin(admin.ModelAdmin):
+    date_hierarchy = 'submission_date'
     list_display= ["name","submitter","spieces","breed","description","sex","age","submission_date"]
+    search_fields = ["name","submitter","spieces","breed","description","sex","age","submission_date"]
+    list_filter = ['spieces']
 
 @admin.register(Vaccine)
 class VaccineAdmin(admin.ModelAdmin):
     pass
+
+admin.site.site_header = "Wisdom Pet Administration"
+admin.site.index_title = ""
+admin.site.site_title = "Wisdom Pet Administration"
--- pythonexample/settings.py
+++ pythonexample/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fipen^nq792!w%+ts^88g+m1sqzh9n+it!=i^tjt$&b+jcr!h2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
